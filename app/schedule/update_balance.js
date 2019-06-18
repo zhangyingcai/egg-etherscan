91fb6597b9fe989110bc9754e8b29567699374dd
mumu <1050675545@qq.com>
2019-06-18 10:28:12 +0800

    更新策略改变

--- app/schedule/update_balance.js
+++ app/schedule/update_balance.js
@@ -4,13 +4,14 @@ class UpdateBalance extends Subscription {
   // 配置信息
   static get schedule(){
     return {
-      interval: '2m',
+      interval: '1m',
       type: 'all'
     }
   }
   
   async subscribe(){
     await this.ctx.service.token.supply(); 
+    await this.ctx.service.token.appsupply();
     this.ctx.service.token.holder();
   }
 }
