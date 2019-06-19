6ea84d183cd2f4137f2a126ea6e2ebbcc6785fb1
mumu <1050675545@qq.com>
2019-06-19 15:10:21 +0800

    Update token.js

--- app/service/token.js
+++ app/service/token.js
@@ -4,7 +4,7 @@ const { supplyurl, balanceurl, txlisturl, apikey, contractaddress, tokenDecimal,
 class TokenService extends Service {
   async update() {
     // 官方地址不计算 计算流通量
-    console.log('-----进入定时任务----');
+    console.log('-----update transaction----');
     try {
       // 组装url get
       const startblock = await this.startblock();
@@ -36,8 +36,15 @@ class TokenService extends Service {
             gasPrice: element.gasPrice, // Gwei
             gasUsed: element.gasUsed
           };
-          // 提交数据
-          this.app.mysql.insert('ethereum', { ...elementData, status: 0 });
+          const theeth = await this.app.mysql.get('ethereum', {hash: element.hash});
+          if (theeth) {
+            // 更新数据
+            this.app.mysql.update('ethereum', { ...elementData }, {where:{id:theeth.id}});
+          }else{
+            // 提交数据
+            this.app.mysql.insert('ethereum', { ...elementData });
+          }
+          
           // 是否是新增地址 -> 新增持有地址
           const created = new Date().getTime() / 1000;
           const from = await this.app.mysql.get('holder', { address: element.from });
@@ -49,11 +56,11 @@ class TokenService extends Service {
             this.app.mysql.insert('holder', { address: element.to, created: created })
           }
           // 结束事务
-          console.log('-----处理完毕----');
         }))
 
         this.updateblock(parseInt(txlistObj.result[txlistObj.result.length - 1].blockNumber) + 1);
-        console.log('-----updateblock----');
+        
+        console.log('-----处理完毕----');
       } else {
         // this.updateblock(Number(endblock)+1);
       }
@@ -79,15 +86,18 @@ class TokenService extends Service {
 
   // update all holders
   async holder() {
-    const timer = new Date().getTime() / 1000 - 24 * 60 * 60;
+    const timer = new Date().getTime() / 1000 - 12 * 60 * 60;
     // 获取中数量
     const res = await this.app.mysql.get('config', { config_name: 'tokenbalance' });
     let balance = 0;
     if (res) {
       balance = res.config_value / Math.pow(10, tokenDecimal);
     }
-
-    const appbalance = (await this.app.mysql.get('config', { config_name: 'appbalance' }) || 0) / Math.pow(10, tokenDecimal);
+    const appres = await this.app.mysql.get('config', { config_name: 'appbalance' });
+    let appbalance = 0;
+    if (appres) {
+      appbalance = appres.config_value / Math.pow(10, tokenDecimal);
+    }
     // 获取数据
     const results = await this.app.mysql.query('SELECT * FROM `holder` WHERE `updated` IS NULL OR `updated` < ' + timer + ' LIMIT 10');
     // const results = await this.app.mysql.select('holder',);
@@ -99,9 +109,6 @@ class TokenService extends Service {
           const newtimer = new Date().getTime() / 1000;
           const num = latestbalance / Math.pow(10, tokenDecimal);
           let percentage = num / (balance - appbalance) * 100;
-          if(item.address === appeth){
-            percentage = '100'; // 官方地址
-          }
           await this.app.mysql.update('holder', { value: latestbalance, updated: newtimer, percentage: percentage, tokenDecimal: tokenDecimal }, { where: { id: item.id } });
         })
       ).then((result) => {
@@ -140,11 +147,11 @@ class TokenService extends Service {
 
   async appsupply(){
     const appbalance = await this.balance(appeth);
-    const isexsit = this.app.mysql.get('config',{config_name:'appbalance'})
+    const isexsit = await this.app.mysql.get('config',{config_name:'appbalance'})
     if(isexsit){
-      await this.app.mysql.update('config', { config_value: appbalance }, { where: { config_name: 'tokenbalance' } });
+      await this.app.mysql.update('config', { config_value: appbalance }, { where: { config_name: 'appbalance' } });
     } else {
-      await this.app.mysql.insert('config', { config_value: appbalance, config_name: 'tokenbalance' });
+      await this.app.mysql.insert('config', { config_value: appbalance, config_name: 'appbalance' });
     }
   }
 
