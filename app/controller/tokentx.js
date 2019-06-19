be59007ab1eb5e7a98df2ef86de9b28b2e349173
mumu <1050675545@qq.com>
2019-06-19 15:10:31 +0800

    Update tokentx.js

--- app/controller/tokentx.js
+++ app/controller/tokentx.js
@@ -10,7 +10,14 @@ class tokentxController extends Controller {
   async index(){
     this.ctx.body = 'hello egg';
   }
-
+  // success
+  success(result) {
+    this.ctx.body = {
+      status: '1',
+      result,
+    };
+  }
+  // 排行榜
   async holders(){
     const ctx = this.ctx;
     
@@ -19,7 +26,8 @@ class tokentxController extends Controller {
     const result = await ctx.service.tokenapi.holders(request);
     ctx.body = result;
   }
-  async token(){
+  // address info
+  async accountinfo(){
     const ctx = this.ctx;
     
     const request = ctx.query;
@@ -29,6 +37,31 @@ class tokentxController extends Controller {
     ctx.validate(rule, request);
     const result = await ctx.service.tokenapi.token(request);
     ctx.body = result;
+  }
+  // hash info
+  async txinfo(){
+    const ctx = this.ctx;
+    
+    const request = ctx.query;
+
+    const rule = {hash: {type:'string'}};
+    
+    ctx.validate(rule, request);
+    const result = await ctx.service.tokenapi.txinfo(request);
+    ctx.body = result;
+  }
+  // token balance
+  async tokensupply(){
+    const ctx = this.ctx;
+    const result = await ctx.service.tokenapi.tokensupply();
+    this.success(result);
+  }
+  // transactions
+  async transactions(){
+    const ctx = this.ctx;
+    const request = ctx.query;
+    const result = await ctx.service.tokenapi.transactions(request);
+    this.success(result);
   }
 }
 
