5d160b220a772ffe91d1e13ebac55e37058bb22b
mumu <1050675545@qq.com>
2019-06-24 10:20:27 +0800

    add info

--- config/config.default.js
+++ config/config.default.js
@@ -1,22 +1,35 @@
 exports.keys = 'damaobcatetherscan';
+
+// etherscan apikey
 exports.apikey = 'K7T921SQKMBW35UTUIVI5YU2VJX76IUQSV';
+// bcat contractaddress
 exports.contractaddress = '0xfDEAA4ab9fea519aFD74df2257A21e5BcA0DFd3f'; // 合约地址
-exports.txlisturl = 'http://api.etherscan.io/api?module=account&action=tokentx'; // token交易列表
+// bcat 交易列表
+exports.txlisturl = 'http://api.etherscan.io/api?module=account&action=tokentx';
+// balance by address
 exports.balanceurl = 'https://api.etherscan.io/api?module=account&action=tokenbalance';
+// supply by contractaddress
 exports.supplyurl = 'https://api.etherscan.io/api?module=stats&action=tokensupply';
-exports.appeth = '0x06bb48363eff79604febc5329dd3688690e7fd8f'; // 官方地址
+// bcat 官方地址
+exports.appeth = '0x06bb48363eff79604febc5329dd3688690e7fd8f'; 
+// bcat 单位
 exports.tokenDecimal = 18;
+
+
+// 单个数据库配置
 exports.mysql = {
   client: {
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: '123456',
-    database: 'etherscan'
+    database: 'test'
   },
   app: true,
   agent: false
 }
+
+// cors  配置  解决跨域
 exports.security = {
   domainWhiteList: [ 'http://localhost' ],
 }
